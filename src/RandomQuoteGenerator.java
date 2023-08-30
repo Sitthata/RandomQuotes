c9043b38f9471db82f4280334de32f23cede8b03
Sitthata <aprils96950@gmail.com>
2023-08-30 23:50:26 +0700

    Added starter quotes

--- src/RandomQuoteGenerator.java
+++ src/RandomQuoteGenerator.java
@@ -1,5 +1,18 @@
 public class RandomQuoteGenerator {
     public static void main(String[] args) {
+        String[] quotes = {
+                "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+                "The future belongs to those who believe in the beauty of their dreams.",
+                "The way to get started is to quit talking and begin doing.",
+                "Life is what happens when you're busy making other plans.",
+                "The world is a book and those who do not travel read only one page.",
+                "Life is either a daring adventure or nothing at all.",
+                "Your time is limited, don't waste it living someone else's life.",
+                "The purpose of our lives is to be happy.",
+                "I find that the harder I work, the more luck I seem to have.",
+                "Don't let yesterday take up too much of today."
+        };
+
         // TODO: Your code here
     }
 }
